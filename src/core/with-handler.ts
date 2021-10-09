7676f8800a2cb7b17c62c16a32d7844aa0ec71f9
neu-trino <neutrino249@gmail.com>
2021-10-09 17:14:41 +0530

    SetHeader method added

--- src/core/with-handler.ts
+++ src/core/with-handler.ts
@@ -1,11 +1,16 @@
 import { NextFunction, Request, Response, Router } from 'express';
 import { RequestHandlerParams } from 'express-serve-static-core';
+import { Header } from './_SetHeader';
 
 export interface ResponseFunction {
   message: string;
   data: {} | string;
   error: {} | string;
   statusCode: number;
+}
+
+type Head = {
+  header: Header
 }
 
 export default (router: Router) => {
@@ -21,20 +26,30 @@ export default (router: Router) => {
       middlewares = [];
     } else if (!middlewares) middlewares = [];
     else if (middlewares instanceof Function) middlewares = [middlewares];
-    routerInstance[type as 'get' | 'post' | 'put'](
+    routerInstance[type as 'get' | 'post' | 'put' | 'delete' | 'patch' | 'head'](
       path,
       ...middlewares,
       async (req: Request, res: Response, next: NextFunction) => {
         try {
-          const handler = (arg: ResponseFunction) => {
+          const handler = (arg: ResponseFunction, headers: Head) => {
             if (arg.statusCode && (arg.data || arg.error || arg.message)) {
-              const data = {
-                message: arg.message,
-                data: arg.data || undefined,
-                error: arg.error,
-              };
-              return res.status(arg.statusCode).json(data);
+              if (headers && headers.header) {
+                for (var key in headers.header) {
+                  res.set(`${key}`, headers.header[key]);
+                }
+              }
+              if (type !== 'head') {
+                const data = {
+                  message: arg.message,
+                  data: arg.data || undefined,
+                  error: arg.error,
+                };
+                return res.status(arg.statusCode).json(data);
+              } else {
+                return res.status(arg.statusCode).end();
+              }
             }
+            return res.sendStatus(500);
           };
           await body(req, handler, res, next);
         } catch (err) {
@@ -48,37 +63,37 @@ export default (router: Router) => {
     postReq: (
       path: string,
       middlewares: RequestHandlerParams | null,
-      body: (req: Request, done: (responseFn: ResponseFunction) => void) => void,
+      body: (req: Request, done: (responseFn: ResponseFunction, setHeader: Header) => void) => void,
     ) => handle(path, router, middlewares, body, 'post'),
 
     getReq: (
       path: string,
       middlewares: RequestHandlerParams | null,
-      body: (req: Request, done: (responseFn: ResponseFunction) => void) => void,
+      body: (req: Request, done: (responseFn: ResponseFunction, setHeader: Header) => void) => void,
     ) => handle(path, router, middlewares, body, 'get'),
 
     putReq: (
       path: string,
       middlewares: RequestHandlerParams | null,
-      body: (req: Request, done: (responseFn: ResponseFunction) => void) => void,
+      body: (req: Request, done: (responseFn: ResponseFunction, setHeader: Header) => void) => void,
     ) => handle(path, router, middlewares, body, 'put'),
 
     deleteReq: (
       path: string,
       middlewares: RequestHandlerParams | null,
-      body: (req: Request, done: (responseFn: ResponseFunction) => void) => void,
+      body: (req: Request, done: (responseFn: ResponseFunction, setHeader: Header) => void) => void,
     ) => handle(path, router, middlewares, body, 'delete'),
 
     patchReq: (
       path: string,
       middlewares: RequestHandlerParams | null,
-      body: (req: Request, done: (responseFn: ResponseFunction) => void) => void,
+      body: (req: Request, done: (responseFn: ResponseFunction, setHeader: Header) => void) => void,
     ) => handle(path, router, middlewares, body, 'patch'),
 
     headReq: (
       path: string,
       middlewares: RequestHandlerParams | null,
-      body: (req: Request, done: (responseFn: ResponseFunction) => void) => void,
+      body: (req: Request, done: (responseFn: ResponseFunction, setHeader: Header) => void) => void,
     ) => handle(path, router, middlewares, body, 'head'),
   };
 };
