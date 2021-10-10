2b169b00ba2ce3cd1b99a52e37e4013244760766
neu-trino <neutrino249@gmail.com>
2021-10-10 22:37:15 +0530

    2.0.3

--- src/core/error-handler.ts
+++ src/core/error-handler.ts
@@ -4,12 +4,7 @@ import { ResponseFunction } from './with-handler';
 
 const errorHandler = ({ debug = false }: { debug: boolean }) => {
   return {
-    handler: (
-      body: (
-        err: Error,
-        done: (responseFn: ResponseFunction) => void
-      ) => void
-    ) => {
+    handler: (body: (err: Error, done: (responseFn: ResponseFunction) => void) => void) => {
       return (err: Error, req: Request, res: Response, next: NextFunction) => {
         if (debug) process.stderr.write(`${err.stack || err.message || err}\n`);
         const handle = (arg: ErrorObj) => {
@@ -19,14 +14,14 @@ const errorHandler = ({ debug = false }: { debug: boolean }) => {
           }
           const data = {
             message: arg.message,
-            error: arg.error
+            error: arg.error,
           };
           return res.status(arg.statusCode).json(data);
-        }
+        };
         body(err, handle);
-      }
-    }
-  }
-}
+      };
+    },
+  };
+};
 
 export default errorHandler;
